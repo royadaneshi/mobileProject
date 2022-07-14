a83bcc5e144b6655e5b99091a674b08254a24a9f
your_username <royadaneshi2001@gmail.com>
2022-07-14 21:01:05 +0430

    translate between languages

---
+++ app/src/main/java/com/edufire/dic3/MainActivity.java
@@ -0,0 +1,87 @@
+package com.edufire.dic3;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.ml.common.modeldownload.FirebaseModelDownloadConditions;
+import com.google.firebase.ml.naturallanguage.FirebaseNaturalLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslateLanguage;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslator;
+import com.google.firebase.ml.naturallanguage.translate.FirebaseTranslatorOptions;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+System.out.println(translateText(getLanguageCode("English"),getLanguageCode("Belarusian"),"dream"));
+    }
+
+
+    public  String translateText(int fromLanguageCode, int toLanguageCode, String source) {
+        final String[] answer = {""};
+        FirebaseTranslatorOptions options =new FirebaseTranslatorOptions.Builder()
+                .setSourceLanguage(fromLanguageCode)
+                .setTargetLanguage(toLanguageCode)
+                .build();
+        FirebaseTranslator translator= FirebaseNaturalLanguage.getInstance().getTranslator(options);
+        FirebaseModelDownloadConditions conditions =new FirebaseModelDownloadConditions.Builder().build();
+        translator.downloadModelIfNeeded(conditions).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void unused) {
+                translator.translate(source).addOnSuccessListener(new OnSuccessListener<String>() {
+                    @Override
+                    public void onSuccess(String s) {
+                        System.out.println(s);
+                        answer[0] =s;
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        System.out.println("Failed to translate");
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                System.out.println("Fail to download the model!++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            }
+        });
+return answer[0];
+    }
+    public  int getLanguageCode(String language) {
+        int languageCode;
+        switch (language) {
+            case "English":
+                languageCode = FirebaseTranslateLanguage.EN;
+                break;
+            case "Afrikaans":
+                languageCode = FirebaseTranslateLanguage.AF;
+                break;
+            case "Arabic":
+                languageCode = FirebaseTranslateLanguage.AR;
+                break;
+            case "Belarusian":
+                languageCode = FirebaseTranslateLanguage.BE;
+                break;
+            case "Czech":
+                languageCode = FirebaseTranslateLanguage.CS;
+                break;
+            case "Hindi":
+                languageCode = FirebaseTranslateLanguage.HI;
+                break;
+            default:
+                languageCode = 0;
+        }
+        return languageCode;
+    }
+
+}
