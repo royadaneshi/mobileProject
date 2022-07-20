35e382c74f774774bc9f8a8e8969e2016ebd7402
Aliiihezaveh <ahezaveh554@gmail.com>
2022-07-20 12:12:36 +0430

    add AdminActivity

--- app/src/main/java/com/edufire/dic3/AdminActivity.java
+++ app/src/main/java/com/edufire/dic3/AdminActivity.java
@@ -1,14 +1,43 @@
 package com.edufire.dic3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.edufire.dic3.Models.User;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 
 public class AdminActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    TextView textView;
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        recyclerView = findViewById(R.id.userRecyclerView);
+
+        UserAdapter userAdapter = new UserAdapter(this);
+        recyclerView.setAdapter(userAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        List<User> users = new ArrayList<>();
+
+        for (Map.Entry<String, User> entry : User.getAllUsers().entrySet()) {
+            users.add(entry.getValue());
+        }
+        textView = findViewById(R.id.userNumber);
+        textView.setText("number of users : " + users.size());
+        userAdapter.setUsers(users);
     }
 }
